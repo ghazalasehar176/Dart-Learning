7ff72a547452c0b0c622b0af77c82ff195809a5a
ghazalasehar176 <aptechsrj176@gmail.com>
2026-02-26 12:06:33 +0500

    Dart Learning

---
+++ class01.dart
@@ -0,0 +1,30 @@
+void main() {
+
+    print("hello world");
+    print("we are learning the dart programming");
+
+    int num1 = 10;
+    print(num1);
+
+    int num2 = 10;
+    print(num2);
+
+    String b = "Hello World";
+    print(b);
+
+    bool c = true;
+    print(c);
+
+    int result = num1 + num2;
+    print(result); 
+
+    if(result != 20){
+        print("correct");
+    }
+    else{
+        print("failed");
+    }
+
+    //void : return no value
+    //main : entry point of the program
+}
